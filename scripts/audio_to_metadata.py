f20e31a0817f281737175400db984c83bd6c58bd
brianfoo <brian@youaremyjoy.org>
2018-08-27 21:56:11 -0400

    implement audio to metadata script with threading

--- scripts/audio_to_metadata.py
+++ scripts/audio_to_metadata.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 
+# Usage: python audio_to_metadata.py -in "../audio/downloads/*.mp3"
+
 import argparse
+import csv
 import glob
 import math
 from mutagen.mp3 import MP3
+from multiprocessing import Pool
+from multiprocessing.dummy import Pool as ThreadPool
+import librosa
 import os
 from pprint import pprint
 import re
@@ -20,16 +26,20 @@ INPUT_FILES = args.INPUT_FILES
 OUTPUT_FILE = args.OUTPUT_FILE
 
 files = glob.glob(INPUT_FILES)
-print("Found %s files" % len(files))
+fileCount = len(files)
+print("Found %s files" % fileCount)
+progress = 0
 
-metadata = []
-for f in files:
+def readFile(f):
+    global progress
     fname = os.path.basename(f)
     basename = fname.split('.')[0]
 
+    # use librosa to get duration
+    y, sr = librosa.load(f)
+    duration = round(librosa.get_duration(y=y, sr=sr), 3)
+
     audio = MP3(f)
-    # print(audio.tags.pprint())
-    length = audio.info.length
 
     # parse filename for metadata
     name = ""
@@ -42,31 +52,64 @@ for f in files:
         description = matches.group(3)
 
     # parse comments for metadata
-    comments = [c.strip() for c in str(audio.tags["COMM::ENG"]).split(";")]
+    comments = []
+    if "COMM::ENG" in audio.tags:
+        comments = [c.strip() for c in str(audio.tags["COMM::ENG"]).split(";")]
     clen = len(comments)
     species = "" if clen <= 0 else comments[0]
-    place = "" if clen <= 1 else comments[1]
-    authors = "" if clen <= 2 else [c.strip() for c in comments[2].split(",")]
+    place = ["", ""] if clen <= 1 else  [c.strip() for c in comments[1].split(",")]
+    authors = [] if clen <= 2 else [c.strip() for c in comments[2].split(",")]
     uid = "" if clen <= 3 else comments[3]
 
+    # parse place
+    state = ""
+    country = ""
+    if len(place) == 2:
+        state, country = tuple(place)
+    elif len(place) == 1:
+        country = place[0]
+
     # parse groups
-    groups = str(audio.tags["TIT1"])
-    groups = groups.replace(", and ", ", ")
-    groups = groups.replace(" and ", ", ")
-    groups = [g.strip() for g in groups.split(",")]
+    groups = []
+    if "TIT1" in audio.tags:
+        groups = str(audio.tags["TIT1"])
+        groups = groups.replace(", and ", ", ")
+        groups = groups.replace(" and ", ", ")
+        groups = [g.strip() for g in groups.split(",")]
 
     entry = {
         "filename": fname,
-        "length": length,
+        "duration": duration,
         "name": name,
         "sample": sampleNumber,
         "description": description,
         "species": species,
-        "place": place,
-        "authors": authors,
+        "state": state,
+        "country": country,
+        "authors": ", ".join(authors),
         "uid": uid,
-        "groups": groups
+        "groups": ", ".join(groups)
     }
-    metadata.append(entry)
     # print(entry)
-    # print "---"
+    # print("---")
+    progress += 1
+    sys.stdout.write('\r')
+    sys.stdout.write("%s%%" % round(1.0*progress/fileCount*100,1))
+    sys.stdout.flush()
+    return entry
+
+pool = ThreadPool()
+metadata = pool.map(readFile, files)
+pool.close()
+pool.join()
+
+print("Writing data to file...")
+entry = metadata[0]
+headings = list(entry.keys())
+with open(OUTPUT_FILE, 'wb') as f:
+    writer = csv.writer(f)
+    writer.writerow(headings)
+    for entry in metadata:
+        writer.writerow([entry[key] for key in headings])
+
+print("Wrote %s rows to %s" % (fileCount, OUTPUT_FILE))
