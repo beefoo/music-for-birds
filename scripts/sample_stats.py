8eb09c9eded7c3332c9f3c1defcb52b2953b7e08
Brian Foo <brian@youaremyjoy.org>
2018-09-05 14:16:25 -0400

    add note octaves to sample stats

--- scripts/sample_stats.py
+++ scripts/sample_stats.py
@@ -3,7 +3,6 @@
 import argparse
 import collections
 import csv
-from matplotlib import pyplot as plt
 import os
 import numpy as np
 from pprint import pprint
@@ -14,10 +13,12 @@ from utils import readCsv
 parser = argparse.ArgumentParser()
 parser.add_argument('-in', dest="INPUT_FILE", default="../data/output/birds_audio_samples.csv", help="Input csv file")
 parser.add_argument('-out', dest="OUTPUT_FILE", default="../data/output/birds_audio_sample_stats.csv", help="Output csv file")
+parser.add_argument('-plot', dest="PLOT", default=0, type=int, help="Whether to show plot")
 args = parser.parse_args()
 
 INPUT_FILE = args.INPUT_FILE
 OUTPUT_FILE = args.OUTPUT_FILE
+PLOT = args.PLOT > 0
 
 print("Reading data file...")
 data = readCsv(INPUT_FILE)
@@ -28,26 +29,35 @@ print("Found %s rows in %s" % (rowCount, INPUT_FILE))
 
 notes = []
 octaves = []
+noteOctaves = []
 x = []
 y = []
 for entry in data:
     notes.append(entry["note"])
     octaves.append(entry["octave"])
+    noteOctaves.append(entry["note"]+str(entry["octave"]))
     x.append(entry["power"])
     y.append(entry["hz"])
 
-print("TOP NOTES:")
+print("NOTES:")
 noteCounter = collections.Counter(notes)
 pprint(noteCounter.most_common())
 print "====="
 
-print("TOP %s OCTAVES:")
+print("OCTAVES:")
 octaveCounter = collections.Counter(octaves)
 pprint(octaveCounter.most_common())
 print "====="
 
+print("NOTES WITH OCTAVES:")
+noteOctaveCounter = collections.Counter(noteOctaves)
+pprint(noteOctaveCounter.most_common(20))
+print "====="
+
 # show scatter plot of data
-plt.scatter(x, y, alpha=0.2)
-plt.xlabel("Power")
-plt.ylabel("Frequency (Hz)")
-plt.show()
+if PLOT:
+    from matplotlib import pyplot as plt
+    plt.scatter(x, y, alpha=0.2)
+    plt.xlabel("Power")
+    plt.ylabel("Frequency (Hz)")
+    plt.show()
